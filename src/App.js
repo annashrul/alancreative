367f895a666518bd98d0db092a38ca0c0059da59
annashrul <anashrulyusuf@gmail.com>
2023-08-09 23:07:23 +0700

    FIX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import Index from "./pages/index";
+export default function App() {
+  // If you want to disable the animation just use the disabled `prop` like below on your page's component
+  // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.     asdsssssssssssssssss
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes >
+          <Route path='/' element={<Index/>} />
+
+          {/*<Route path="/">*/}
+          {/*<Index />*/}
+        {/*</Route>*/}
+      </Routes >
+    </Router>
   );
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
---
+++ src/redux/type.js
@@ -0,0 +1,11 @@
+export const AUTH = {
+  URL: "https://annashrulyusuf.000webhostapp.com/api/",
+  IMG:"https://annashrulyusuf.000webhostapp.com/storage"
+};
+
+export const PRODUCT = {
+  GET: "GET",
+  PAGINATION: "PAGINATION",
+  GET_LOADING: "GET_LOADING",
+  POST_LOADING: "POST_LOADING",
+};
